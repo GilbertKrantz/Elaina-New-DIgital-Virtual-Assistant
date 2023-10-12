1107ed4628f4f98317f0ddb4bcfa857b8bf277d8
ElTheHuman <145774211+ElTheHuman@users.noreply.github.com>
2023-10-12 11:33:48 +0700

    Update gpt-llm.py
    Model 0.1

--- Models/gpt-llm.py
+++ Models/gpt-llm.py
@@ -1,12 +1,12 @@
 import openai
 
-openai.api_key = "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"
+openai.api_key = "[open-ai-api-key]"
 
 messageHistory = []
 
 def chat(input:str, role = "user"):
     messageHistory.append({"role": role, "content": input})
-    completion = openai.ChatCompletion.create(model = "gpt-3.5-turbo", messages = messageHistory)
+    completion = openai.ChatCompletion.create(model = "[gptmodel]", messages = messageHistory)
     reply = completion.choices[0].message.content
     messageHistory.append({"role": "assistant", "content": reply})
 
