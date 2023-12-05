b6df6fe247f8125578cc5fdc51c63a99e9c76d63
ElTheHuman <145774211+ElTheHuman@users.noreply.github.com>
2023-12-05 16:30:17 +0700

    Models-0.1

---
+++ Models/cai-llm.py
@@ -0,0 +1,28 @@
+from characterai import PyCAI
+
+client = PyCAI('[user-token]')
+
+char = '[character-token]'
+
+chat = client.chat.get_chat(char)
+
+participants = chat['participants']
+
+if not participants[0]['is_human']:
+    tgt = participants[0]['user']['username']
+else:
+    tgt = participants[1]['user']['username']
+
+while True:
+    message = input('You: ')
+
+    data = client.chat.send_message(
+        chat['external_id'], tgt, message
+    )
+
+    name = data['src_char']['participant']['name']
+    text = data['replies'][0]['text']
+
+    print(f"{name}: {text}")
+
+# Get token: JSON.parse(localStorage.getItem('char_token')).value in console
